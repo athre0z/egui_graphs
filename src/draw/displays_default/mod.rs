04ea10bf84a81a7be42ce168cd22cf0e25034046
Joel Höner <joel@elastic.co>
2024-05-13 20:56:09 +0200

    fixup: prevent crashes on NaN location

--- src/draw/displays_default/mod.rs
+++ src/draw/displays_default/mod.rs
@@ -12,5 +12,10 @@ pub use node::DefaultNodeShape;
 /// some platforms we have seen crashes from allocating those textures starting at sizes 
 /// around 2000. Values >200 already take very long to generate (multiple seconds).
 fn clamp_font_size(size: f32) -> f32 {
-    size.clamp(1.0, 128.0)
+    if size.is_finite() {
+        size.clamp(1.0, 128.0)
+    } else {
+        // Infinity and NaN values tend to crash the font renderer: default to `1.0`.
+        1.0
+    }
 }
